918ae78af6b1343e923e13e631771592e931918c
Anna Russi <annarussi@gmail.com>
2023-05-31 21:14:49 +0100

    start the process to add the map at the show view

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,5 +4,8 @@ class Board < ApplicationRecord
 
   has_one_attached :photo
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   validates :title, :price_per_day, :location, :photo, presence: true
 end
