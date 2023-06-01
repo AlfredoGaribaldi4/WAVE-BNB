0d88038f5cd3d2382cf07ac37cf6e16a381a9cd8
Alfredo Garibaldi <alfredogaribaldi46@gmail.com>
2023-06-01 14:23:36 +0100

    helping francisco with validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,4 +6,11 @@ class Booking < ApplicationRecord
   enum status: %i[pending confirmed declined]
 
   validates :date_check_in, :date_check_out, presence: true
+  validate :user_cannot_book_own_product
+
+  def user_cannot_book_own_product
+    if user && board.user == user
+      errors.add(:user, "cannot book their own product")
+    end
+  end
 end
